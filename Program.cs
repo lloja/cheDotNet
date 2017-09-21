9942c4a321ccf797127e4dc88288525b93ab70b1
lloja <luis.loja@softwareag.com>
2017-09-21 11:57:46 +0000

    Proper project structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cheDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
