7de318fe5a76058c82a525e83ec36efc9bed6065
lloja <luis.loja@softwareag.com>
2017-09-21 16:20:11 +0000

    A different way to calculate Fib

--- Program.cs
+++ Program.cs
@@ -6,38 +6,11 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0)
+            var generator = new FibonacciGenerator();
+            foreach (var digit in generator.Generate(15))
             {
-                Console.WriteLine($"Hello {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
-
-            Console.WriteLine("Fibonacci Numbers 1-15:");
-
-            for (int i = 0; i < 15; i++)
-            {
-                Console.WriteLine($"{i + 1}: {FibonacciNumber(i)}");
+                Console.WriteLine(digit);
             }
         }
-
-        static int FibonacciNumber(int n)
-        {
-            int a = 0;
-            int b = 1;
-            int tmp;
-
-            for (int i = 0; i < n; i++)
-            {
-                tmp = a;
-                a = b;
-                b += tmp;
-            }
-
-            return a;
-        }
-
     }
 }
